5b340c21f6d87813338c2c0dd2756d9b866a210d
Chris Dawson <xrdawson@gmail.com>
2019-08-12 12:37:20 -0700

    Try to prevent ie11 from caching

--- test/address_test.js
+++ test/address_test.js
@@ -4,12 +4,12 @@ const expect = require('expect');
 describe('#address', () => {
   describe('#convertPathToAddress', () => {
     it('get back the correct address for rtp', () => {
-      const address = convertPathToAddress("rtp__239_0_0_1__1234");
+      const address = convertPathToAddress('rtp__239_0_0_1__1234');
       expect(address).toBe('rtp://239.0.0.1:1234');
     });
 
     it('get back the correct address for udp', () => {
-      const address = convertPathToAddress("udp__239_0_0_1__1234");
+      const address = convertPathToAddress('udp__239_0_0_1__1234');
       expect(address).toBe('udp://239.0.0.1:1234');
     });
   });
