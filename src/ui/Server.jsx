4f2e318237449494474e5ef510172a870a9b38f9
Chris Dawson <xrdawson@gmail.com>
2019-10-04 23:04:05 -0700

    Fix tests

--- src/ui/Server.jsx
+++ src/ui/Server.jsx
@@ -4,17 +4,21 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const { Text, Color, Box } = require('ink');
 
-const Server = ({server}) => {
-  const { on, config } = server;
-
-  return (
-    <Box height={3} flexDirection="column">
-    <Text>Server status: {on ? <Color green>listening on { config.port }</Color> : <Color red>off</Color>}</Text>
-    { on && 
-    <Text>HLS: <Color blue>http://localhost:{ config.port }/hls.html?s={config.url}</Color></Text>
-    }
-    </Box>
-  );
+const Server = (props) => {
+  const { server} = props;
+  if (!server) {
+    return null;
+  } else {
+    const { on, config } = server;
+    return (
+      <Box height={3} flexDirection="column">
+      <Text>Server status: {on ? <Color green>listening on { config.port }</Color> : <Color red>off</Color>}</Text>
+      { on && 
+      <Text>HLS: <Color blue>http://localhost:{ config.port }/hls.html?s={config.url}</Color></Text>
+      }
+      </Box>
+    );
+  }
 };
 
 Server.propTypes = {
--- test/output_test.js
+++ test/output_test.js
@@ -9,14 +9,18 @@ const stripAnsi = require('strip-ansi');
 describe('#ui', () => {
   describe('#Server', () => {
     it('should display off when nothing is provided', () => {
-      const {lastFrame} = render(React.createElement(Server));
+      const {lastFrame} = render(React.createElement(Server, { server: {} }));
       const rendered = stripAnsi(lastFrame());
-      expect(rendered).toEqual('Status: off');
+      expect(rendered).toEqual('Server status: off\n\n');
     });
     it('should display on when server is on', () => {
-      const {lastFrame} = render(React.createElement(Server, {on: true}));
+      const config = { url: 'udp://239.0.0.1:1234', port: 4567};
+      const {lastFrame} = render(React.createElement(Server, {server: { on: true, config }}));
       const rendered = stripAnsi(lastFrame());
-      expect(rendered).toEqual('Status: on');
+      const msg = `Server status: listening on 4567
+HLS: http://localhost:4567/hls.html?s=udp://239.0.0.1:1234
+`;
+      expect(rendered).toEqual(msg);
     });
   });
 
