f5202eeacb7fa9ebc23e78300012803ac490670a
Chris Dawson <xrdawson@gmail.com>
2019-10-09 22:32:47 -0700

    Rewrite the playlist when we have it

--- src/playlist.js
+++ src/playlist.js
@@ -8,41 +8,64 @@ const RETRY_MAX = 60;
 
 module.exports.setConfig = (cfg) => config = cfg;
 
+const rewriteM3u8File = module.exports.rewriteM3u8File = (contents, programId) => {
+  const lines = contents.split('\n');
+  const rewritten = [];
+  lines.forEach( (l) => {
+    if (l.endsWith('.ts')) {
+      rewritten.push( `${programId}/${l}`);
+    } else {
+      rewritten.push(l);
+    }
+  });
+  return rewritten.join('\n');
+};
+
 const sendRedirectFile =
-  module.exports.sendRedirectFile = (fixedDirectory, res) => {
+  module.exports.sendRedirectFile = (fixedDirectory, res, programId) => {
     const redirect = path.join(fixedDirectory, 'redirect.m3u8');
     const contents = fs.readFileSync(redirect).toString();
+    const rewitten = rewriteM3u8File(contents, programId);
     res.set('Content-Type', mime.M3U8_MIME);
-    res.send(contents);
+    res.send(rewritten);
   };
 
 
 const ifTsFilesAreReadyThenSend =
   module.exports.ifTsFilesAreReadyThenSend =
     ({config, url, res, index = 0}) => {
-      dirUtils
-          .isReadyToView(config.fixedDirectory)
-          .then( (ready) => {
-            if (ready) {
-              o.updateSegmenter(url, {status: 'ready'});
-              sendRedirectFile(config.fixedDirectory, res);
-            } else {
-              if (index < RETRY_MAX) {
-                setTimeout(() => {
-                  ifTsFilesAreReadyThenSend(
-                      {config, url, res,
-                        index: index + 1});
-                }, 1000);
-                o.updateSegmenter(url, {status: 'holding'});
+      // TODO: need to check for the PID inside the fixed Directory
+      // Better yet, add the correct directory+pid to the program?
+      const program = config.programs.find((p) => p.url === url);
+      const programDir = `${config.fixedDirectory}/${program.programId}`;
+      if (program) {
+        dirUtils
+            .isReadyToView(programDir)
+            .then( (ready) => {
+              if (ready && serverStatus.on) {
+                o.updateSegmenter(url, {status: 'ready'});
+                sendRedirectFile(programDir, res);
               } else {
-                res.send('Error, timed out.');
+                if (serverStatus.on && index < RETRY_MAX) {
+                  setTimeout(() => {
+                    ifTsFilesAreReadyThenSend(
+                        {config, url, res,
+                          index: index + 1});
+                  }, 1000);
+                  o.updateSegmenter(url, {status: 'holding'});
+                } else {
+                  res.send('Error, timed out.');
+                }
               }
-            }
-          })
-          .catch((err) => {
-            o.errors('Error: ' + err.toString());
-            throw err;
-          });
+            })
+            .catch((err) => {
+              o.errors('Error: ' + err.toString());
+              throw err;
+            });
+      } else {
+        o.errors('FATAL Error: No program found');
+        throw new Error('FATAL ERROR: No program found');
+      }
     };
 
 module.exports.sendBackPlaylistWhenReady = ({config, url, res}) => {
